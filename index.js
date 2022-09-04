f2f9698360e7ae7643a7cb0121d7cf5c5fc82710
vijayalakshmiKaruppusamy <vijayalakshmi.k@moonraft.com>
2022-09-04 10:57:09 +0530

    project

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ document.querySelectorAll(".linknumber").forEach(n  => n .addEventListener("clic
 function validate()
 {
     var mail = document.getElementById("email").value;
-    var emailcondition = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var emailcondition = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if(mail.match(emailcondition))
     {
         alert(mail);
@@ -24,3 +24,42 @@ function validate()
         document.getElementById("result").innerHTML = "Invalid Email-id";
     }
 }
+
+let horizontalimage = [
+    {
+       image : "Pictures/unsplash_fbAnIjhrOL4.png",
+       imagename : "Fiddle leaf fig",
+       price : "$110.99"
+    },
+    {
+        image : "Pictures/unsplash_T0inbt7nRME.png",
+        imagename : "Aloe Vera fig",
+        price : "$110.99"
+     },
+     {
+        image : "Pictures/unsplash_Ebwp2-6BG8E.png",
+        imagename : "Strelitzia nicolai fig",
+        price : "$110.99"
+     },
+     {
+        image : "Pictures/unsplash_fbAnIjhrOL4.png",
+       imagename : "Ficus microcarpa fig",
+        price : "$110.99"
+     },
+     {
+        image : "Pictures/unsplash_T0inbt7nRME.png",
+       imagename : "Aloe Vera fig",
+        price : "$110.99"
+     }] ;
+
+     var key="";
+     for(var i=0;i<horizontalimage.length;i++)
+     {   
+             key += '<div class="horizontalimage">'   
+             key += '<div class= "images"><img src = '+ horizontalimage[i].image + '></div>';
+             key += '<div class= "caption">';
+             key+= '<div class="image_name">' + horizontalimage[i].imagename + '</div>';
+             key+= '<div class="price">' + horizontalimage[i].price + '</div></div></div>';
+     }
+     console.log(key);
+     document.getElementById("horizontalimage").innerHTML = key;
